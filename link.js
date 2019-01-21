a08174352ac684eafa7029dae9fc4ff4bf2587b5
pretty3831 <46170310+pretty3831@users.noreply.github.com>
2019-01-21 23:28:58 +0200

    Update link.js

--- link.js
+++ link.js
@@ -26,5 +26,26 @@ const client = new Discord.Client();
     }
 }); 
 
+client.on('ready', function(){
+    var ms = 10000 ;
+    var setGame = [' 1help','GaMerZ Community','Have Fun !',' Iam Here For Games',' 1help'];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/one`);
+    }, ms);
+  console.log('---------------');
+  console.log('GaMerZ -Bot Is Online')
+  console.log('---------------')
+});
+
+
 
 client.login("NTM2OTkxMzMzMTUyNDU2NzI1.DyfA2Q.hLjrNRaz3swRUl4iXuPBbs69-zc")
